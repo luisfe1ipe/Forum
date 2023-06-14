4c6bdf8e8514e01c8636506247eaed0e7918fb37
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2023-06-14 19:51:01 -0300

    user registration

---
+++ ForumApi/app/Http/Controllers/Auth/AuthenticationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticationController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+        $token = $user->createToken("forumapp")->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+}
